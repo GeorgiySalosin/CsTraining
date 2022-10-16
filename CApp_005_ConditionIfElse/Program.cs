6c0f304a17b2ded00d2b5d740f6d54fccc974384
George Salosin <gsalosin@gmail.com>
2022-10-16 15:22:49 +0700

    Conditions

---
+++ CApp_005_ConditionIfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Username: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "gosha") 
+{
+    Console.WriteLine ("Hello:3");
+}
+else
+{
+    Console.WriteLine ("Welcome, " + username);
+}
